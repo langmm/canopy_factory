40f9d06a21b24c1ed23e3da874e50e12890c960e
Meagan Lang <langmm.astro@gmail.com>
2025-09-11 15:20:15 -0400

    Install yggdrasil from source

---
+++ build_yggdrasil.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+if [ ! -d yggdrasil ]; then
+    git clone --branch topic/cache https://github.com/cropsinsilico/yggdrasil.git
+fi
+cd yggdrasil
+pip install . --no-build-isolation
