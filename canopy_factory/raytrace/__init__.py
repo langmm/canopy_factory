Fix rounding error in image resolution calculation Add test data for second raytrace timestep in totals to determine if the difference arises from the raytracer or from the totals calculation

canopy_factory/raytrace/__init__.py
@@ -2238,7 +2238,7 @@ class AnimateTask(TemporalTaskBase):
             self._inset_figure = self.totals_task.generate_output(
                 'totals_plot')
             width_px = old_data.shape[1]
-            height_px = int(0.2 * width_px)
+            height_px = int(np.round(0.2 * width_px))
             dpi = self._inset_figure.get_dpi()
             figsize = ((width_px + 1) / dpi, (height_px + 1) / dpi)
             self._inset_figure.set_size_inches(*figsize)

canopy_factory/raytrace/base.py
@@ -611,7 +611,7 @@ class RayTracerBase(RegisteredClassBase):
         r"""int: Number of pixels in the x direction."""
         if ((self.args.resolution is not None
              or self.args.image_nx is None)):
-            return int(self.image_width * self.resolution)
+            return int(np.round((self.image_width * self.resolution).value))
         return self.args.image_nx
 
     @cached_args_property
@@ -620,7 +620,7 @@ class RayTracerBase(RegisteredClassBase):
         if ((self.args.resolution is not None
              or (self.args.image_ny is None
                  and self.args.image_nx is not None))):
-            return int(self.image_height * self.resolution)
+            return int(np.round((self.image_height * self.resolution).value))
         elif self.args.image_ny is not None:
             return self.args.image_ny
         return 2048